f0baf0a8f484818c553c5f0a504c46dba9dcba5b
Guedes <gguilhermegguedes@gmail.com>
2022-04-05 12:58:59 -0300

    implementado alguns exemplos

--- src/index.js
+++ src/index.js
@@ -19,6 +19,17 @@ function verifyIfExistsAccountCPF(request, response, next){
     return next();
 }
 
+function getBalance(statement){
+    
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === "credit"){
+            return acc + operation.amount;
+        }else{
+            return acc - operation.amount;
+        }
+    }, 0)
+    return balance;
+}
 
 const customers = [];
 
@@ -42,22 +53,28 @@ app.post("/account", (request, response) => {
 });
 
 
-app.get("/statement", verifyIfExistsAccountCPF,(request, response) => {
+app.get("/account", verifyIfExistsAccountCPF,(request, response) => {
     const { customer } = request;
     
-    return response.json(customer.statement);
+    return response.json(customer);
 })
 
-app.put("/", (request, response) => {
-    
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const {customer} = request;
+    const {name} = request.body;
+
+    customer.name = name;
+
+    return response.status(201).send();
 })
 
-app.patch("/", (request, response) => {
-    
-})
 
-app.delete("/", (request, response) => {
-    
+app.delete("/account", verifyIfExistsAccountCPF,(request, response) => {
+    const { customer } = request;
+
+    customers.splice(customer,1);
+
+    return response.status(201).send();
 })
 
 
@@ -79,7 +96,40 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
 });
 
 
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const {customer} = request;
 
+    const balance = getBalance(customer.statement);
+    
+    if (balance < amount){
+        return response.status(400).json({error: "Insufucient balance !"});
+    }
 
+    const statementOperation = {
+        amount: balance,
+        created_at: new Date(),
+        type: "Debit"
+    }
+    customer.statement.push(statementOperation);
+    
+    return response.status(201).json("Successful withdraw !");
+});
+
+app.get("/statement", verifyIfExistsAccountCPF,(request, response) => {
+    const { customer } = request;
+    
+    return response.json(customer.statement);
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF,(request, response) => {
+    const { customer } = request;
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00");
+    const statement = customer.statement.filter((statment) => statment.created_at.toDateString() === new Date(dateFormat).toDateString())
+    
+    return response.json(statement);
+})
 
 app.listen(3333);
